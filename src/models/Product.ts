220524005524539ad43cc5e69af7425c2d2638ff
HanamiLux <oxotnikzn2017@yandex.ru>
2025-04-15 10:17:28 +0300

    enhancement: order status

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -2,33 +2,18 @@ import {CategoryDto} from "./Category";
 import {OrderedProductDto} from "./orderedProduct";
 import {FeedbackDto} from "./Feedback";
 
-// export interface ProductDto {
-//     productId: number;
-//     productName: string;
-//     description: string;
-//     price: number;
-//     quantity: number;
-//     categoryId: number;
-//     imagesUrl: string[];
-//     averageRating: number;
-//     totalFeedbacks: number;
-//     category: CategoryDto;
-//     orderedProducts: OrderedProductDto[];
-//     feedbacks: FeedbackDto[];
-// }
-
 export interface ProductDto {
     productId: number;
     productName: string;
     description: string;
     price: number;
     quantity: number;
+    categoryId: number;
+    imagesUrl: string[];
     averageRating: number;
     totalFeedbacks: number;
-    imagesUrl?: string[];
-    category: {
-        categoryId: number;
-        categoryName: string;
-    };
+    category: CategoryDto;
+    orderedProducts: OrderedProductDto[];
+    feedbacks: FeedbackDto[];
 }
 
